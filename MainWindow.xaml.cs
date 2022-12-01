469b5995cc2ed1cb21593328c882fd611bf5dc4d
so19079 <so19079@DTGS2241206D01>
2022-12-01 14:18:14 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BC = BCrypt.Net.BCrypt;
+
+namespace bcrypt_test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_Button_Click(object sender, RoutedEventArgs e)
+        {
+            tbx_hashedPWD.Text = BC.HashPassword(tbx_password.Text);
+        }
+
+        private void Check_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            MessageBox.Show(
+                BC.Verify(tbx_password_to_check.Text, tbx_hashedPWD.Text)
+                ? "Passwords match"
+                : "Passwords don't match"
+            );
+
+            /*
+            string displayString;
+
+            if(BC.Verify(tbx_password_to_check.Text, tbx_hashedPWD.Text))
+            {
+                displayString = "Passwords match";
+            }
+            else
+            {
+                displayString = "Passwords don't match";
+            }
+            
+            
+            MessageBox.Show(displayString);
+            */
+        }
+    }
+}
---
+++ bcrypt_testContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace bcrypt_test
+{
+    public partial class bcrypt_testContext : DbContext
+    {
+        public bcrypt_testContext()
+        {
+        }
+
+        public bcrypt_testContext(DbContextOptions<bcrypt_testContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseMySql("server=localhost;database=bcrypt_test;user=root", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.4.24-mariadb"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("utf8mb4_general_ci")
+                .HasCharSet("utf8mb4");
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("users");
+
+                entity.Property(e => e.Id).HasColumnType("int(11)");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(255)
+                    .HasColumnName("email");
+
+                entity.Property(e => e.Username)
+                    .HasMaxLength(255)
+                    .HasColumnName("username");
+
+                entity.Property(e => e.Wachtwoord)
+                    .HasMaxLength(100)
+                    .HasColumnName("wachtwoord");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
